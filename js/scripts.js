9fb76a8cc2680a5cb1ecd7368ba41bcf55ed313c
YamilYanez <yamilenriqueyanezmorales@hotmail.com>
2016-10-07 13:09:14 -0500

    Reestructuracion
    Correcion de contenedores, like + fecha, jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
+function mysqlDateFormat(date) {
+    var isoDate = date.toISOString();
+    var dateNT = isoDate.split("T").shift();
+    var pieces = dateNT.split('-');
+    pieces.reverse();
+    return pieces.join('-');
+}
+
+jQuery(document).ready(function() {
+    $(".publicacion > div:first-of-type div img").click(function() {
+        var idPubli = $(this).closest('.publicacion').attr('id');
+        $("#" + idPubli + " > div:first-of-type div img").attr('src', '../Images/lovingIcon.png');
+    });
+});
+
+
 function GetElementInsideContainer(containerID, childID) {
     var elm = document.getElementById(childID);
     var parent = elm ? elm.parentNode : {};
@@ -43,22 +59,26 @@ function Post() {
     var title = $('#ingresar div:last-child input').get(0).value;
     var description = $("#ingresar div:last-child textarea").get(0).value;
     var mediaSource = $("#ingresar input[type = 'file']").get(0).value;
-    CreatePost("Images/loginBackground2.jpg", title, description);
+    CreatePost("../Images/loginBackground2.jpg", title, description);
 }
 
 function CreatePost(mediaSource, title, description) {
     var columna = document.getElementById("columna" + columnaActual);
     var oldHtml = columna.innerHTML;
     columna.innerHTML = "" +
-            "<div class='publicacion' id='publicacion_" + idPublicacion + "'>" +
+        "<div class='publicacion' id='publicacion_" + idPublicacion + "'>" +
             "<img src=" + mediaSource + " alt='media not found'>" +
             "<h4 class='Title'>" + title + "</h4>" +
             "<h5 class='Description'>" + description + "</h5>" +
             "<hr class='Separador'>" +
-            "<h6>Comentarios</h6>" +
             "<div>" +
+                "<span>" + mysqlDateFormat(new Date()) + "</span>" +
+                "<div><img src='../Images/LoveIcon.png' alt='not found'><span>(count)</span></div>" +
+            "</div>" + 
+            "<h6>Comentarios</h6>" +
+            "<div id='commentBox'>" +
                 "<div>" +
-                    "<img src='Images/loginBackground2.jpg' alt='not found'>" +
+                    "<img src='../Images/loginBackground2.jpg' alt='not found'>" +
                     "<textarea rows=2></textarea>" +
                 "</div>" +
                 "<div>" +
@@ -75,7 +95,7 @@ function CreatePost(mediaSource, title, description) {
 
 function Notificate(type, idUsuario, idPubli) {
     var newItem = document.createElement("li");
-    newItem.innerHTML = "<img src='Images/NotificationIcon.png' alt='not found'>" +
+    newItem.innerHTML = "<img src='../Images/NotificationIcon.png' alt='not found'>" +
                         "<p>UsuarioX ha X.</p>";
 
     var list = document.getElementById("notificacion");  
@@ -86,7 +106,7 @@ function LoadComment(idPubli, imgPerfil, usuario, comentario, fecha, hora) {
     var idCom = idPubli.split("_").pop();
     GetElementInsideContainer(idPubli, "comentarios_" + idCom).innerHTML += "" +
         "<div class='comentario'>" +
-            "<img src='Images/loginBackground2.jpg' alt='not found'>" + 
+            "<img src='../Images/loginBackground2.jpg' alt='not found'>" + 
             "<div>" +
                 "<a href=''>" +
                 "<p>" + usuario + "</p>" +
